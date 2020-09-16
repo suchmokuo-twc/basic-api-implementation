eaaf13353a83526015a789966761f3f3b155c5a0
SUCHMOKUO <suchmokuo@gmail.com>
2020-09-16 20:52:54 +0800

    [liuxing] implement user age non-null validation

--- src/main/java/com/thoughtworks/rslist/dto/User.java
+++ src/main/java/com/thoughtworks/rslist/dto/User.java
@@ -7,6 +7,7 @@ import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 @Data
@@ -21,6 +22,7 @@ public class User {
     @Size(max = 8)
     private String userName;
 
+    @NotNull
     private Integer age;
 
     @NotEmpty
--- src/test/java/com/thoughtworks/rslist/api/RsControllerTest.java
+++ src/test/java/com/thoughtworks/rslist/api/RsControllerTest.java
@@ -231,4 +231,27 @@ class RsControllerTest {
 
         assertNotEquals(mvcResult.getResponse().getStatus(), OK.value());
     }
+
+    @Test
+    void should_not_create_rs_event_when_empty_user_age() throws Exception {
+        String newRsEventJson = RsEvent.builder()
+                .eventName("name")
+                .keyword("key1")
+                .user(User.builder()
+                        .userName("abc")
+                        .age(null)
+                        .gender("male")
+                        .email("abc@twc.com")
+                        .phone("10000000000")
+                        .build())
+                .build()
+                .toJson();
+
+        MvcResult mvcResult = mockMvc.perform(post("/rs/events")
+                .content(newRsEventJson)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andReturn();
+
+        assertNotEquals(mvcResult.getResponse().getStatus(), OK.value());
+    }
 }
