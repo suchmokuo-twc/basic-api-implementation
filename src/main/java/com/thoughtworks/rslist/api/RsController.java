a9ae233140e364448dc9d3da7e2b3e7650e851cd
SUCHMOKUO <suchmokuo@gmail.com>
2020-09-16 22:27:56 +0800

    [liuxing] refactor: modify indent

--- src/main/java/com/thoughtworks/rslist/api/RsController.java
+++ src/main/java/com/thoughtworks/rslist/api/RsController.java
@@ -21,55 +21,55 @@ import java.util.List;
 @RequestMapping(path = "/rs", produces = "application/json; charset=utf-8")
 public class RsController {
 
-  @Autowired
-  private UserService userService;
+    @Autowired
+    private UserService userService;
 
-  private final List<RsEvent> rsList;
+    private final List<RsEvent> rsList;
 
-  public RsController() {
-    rsList = new ArrayList<RsEvent>() {{
-      add(RsEvent.builder().eventName("第一条事件").keyword("无分类").build());
-      add(RsEvent.builder().eventName("第二条事件").keyword("无分类").build());
-      add(RsEvent.builder().eventName("第三条事件").keyword("无分类").build());
-    }};
-  }
-
-  @GetMapping("/events")
-  public List<RsEvent> getAllRsEvents() {
-    return rsList;
-  }
-
-  @PostMapping("/events")
-  public RsEvent createRsEvent(@Valid @RequestBody RsEvent rsEvent) {
-    User user = rsEvent.getUser();
-    userService.registerUser(user);
-    rsList.add(rsEvent);
-    return rsEvent;
-  }
-
-  @PutMapping("/events/{index}")
-  public RsEvent updateRsEvent(@RequestBody RsEvent rsEvent, @PathVariable int index) {
-    index--;
-    validateIndex(index);
-
-    RsEvent oldRsEvent = rsList.get(index);
-    RsEvent newRsEvent = oldRsEvent.merge(rsEvent);
-    rsList.set(index, newRsEvent);
-
-    return newRsEvent;
-  }
-
-  @DeleteMapping("/events/{index}")
-  public RsEvent deleteRsEvent(@PathVariable int index) {
-    index--;
-    validateIndex(index);
-
-    return rsList.remove(index);
-  }
-
-  private void validateIndex(int index) {
-    if (index < 0 || index >= rsList.size()) {
-      throw new RuntimeException("index out of range");
+    public RsController() {
+        rsList = new ArrayList<RsEvent>() {{
+            add(RsEvent.builder().eventName("第一条事件").keyword("无分类").build());
+            add(RsEvent.builder().eventName("第二条事件").keyword("无分类").build());
+            add(RsEvent.builder().eventName("第三条事件").keyword("无分类").build());
+        }};
     }
-  }
+
+    @GetMapping("/events")
+    public List<RsEvent> getAllRsEvents() {
+        return rsList;
+    }
+
+    @PostMapping("/events")
+    public RsEvent createRsEvent(@Valid @RequestBody RsEvent rsEvent) {
+        User user = rsEvent.getUser();
+        userService.registerUser(user);
+        rsList.add(rsEvent);
+        return rsEvent;
+    }
+
+    @PutMapping("/events/{index}")
+    public RsEvent updateRsEvent(@RequestBody RsEvent rsEvent, @PathVariable int index) {
+        index--;
+        validateIndex(index);
+
+        RsEvent oldRsEvent = rsList.get(index);
+        RsEvent newRsEvent = oldRsEvent.merge(rsEvent);
+        rsList.set(index, newRsEvent);
+
+        return newRsEvent;
+    }
+
+    @DeleteMapping("/events/{index}")
+    public RsEvent deleteRsEvent(@PathVariable int index) {
+        index--;
+        validateIndex(index);
+
+        return rsList.remove(index);
+    }
+
+    private void validateIndex(int index) {
+        if (index < 0 || index >= rsList.size()) {
+            throw new RuntimeException("index out of range");
+        }
+    }
 }
